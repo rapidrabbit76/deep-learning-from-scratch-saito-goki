c109452f3fc12b5c642c708207ffaa14b2b1c49a
yslee <yslee.dev@gmail.com>
2022-12-14 21:38:57 +0900

    progress: ch 3 done

---
+++ 3rd/dezero/functions.py
@@ -0,0 +1,48 @@
+import numpy as np
+import dezero
+from dezero.core import Function, Variable, as_variable, as_array
+
+
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        gx = gy * cos(x)
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+
+def cos(x):
+    return Cos()(x)
+
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = gy * (1 - y * y)
+        return gx
+
+
+def tanh(x):
+    return Tanh()(x)
